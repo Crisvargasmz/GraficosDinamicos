1f571c8ae189d83cce49b2ee0a7b8cfde701b0cb
Crisvargasmz <cristhiancesarvargas12@gmail.com>
2024-10-08 08:45:24 -0600

    Grafico calendario count

--- App.js
+++ App.js
@@ -4,11 +4,39 @@ import GraficoSalarios from './src/components/GraficoSalarios';
 import GraficoGeneros from './src/components/GraficoGenero';
 import Formulario from './src/components/Formulario';
 import { collection, getDocs, query } from 'firebase/firestore';
+import GraficoReporteEnfermedades from './src/components/GraficoReporteEnfermedades';
 
 //Importación de conexión a firebase
 import db from './database/firebaseconfig';
 
 export default function App() {
+
+  const dataReporteEnfermedades = [
+    { date: "2017-01-05", count: 8 }, 
+    { date: "2017-01-19", count: 5 }, 
+    { date: "2017-02-06", count: 2 }, 
+    { date: "2017-02-20", count: 4 }, 
+    { date: "2017-03-07", count: 1 }, 
+    { date: "2017-03-21", count: 3 }, 
+    { date: "2017-04-05", count: 6 }, 
+    { date: "2017-04-19", count: 2 }, 
+    { date: "2017-05-03", count: 4 },
+    { date: "2017-05-17", count: 7 },
+    { date: "2017-06-06", count: 9 }, 
+    { date: "2017-06-20", count: 5 }, 
+    { date: "2017-07-05", count: 3 }, 
+    { date: "2017-07-19", count: 4 }, 
+    { date: "2017-08-07", count: 2 },  
+    { date: "2017-08-21", count: 8 },  
+    { date: "2017-09-06", count: 3 },
+    { date: "2017-09-20", count: 7 },
+    { date: "2017-10-04", count: 5 },
+    { date: "2017-10-18", count: 6 },
+    { date: "2017-11-06", count: 2 },
+    { date: "2017-11-20", count: 9 }, 
+    { date: "2017-12-05", count: 4 },
+    { date: "2017-12-19", count: 7 } 
+  ];
 
   const [bandera, setBandera] = useState(false); // Variable bandera
   const [dataSalarios, setDataSalarios] = useState({
@@ -104,12 +132,24 @@ export default function App() {
         <Formulario setBandera={setBandera}/>
         <GraficoSalarios dataSalarios={dataSalarios}/>
         <GraficoGeneros dataGeneros={dataGeneros}/>
+        <GraficoReporteEnfermedades dataReporteEnfermedades={dataReporteEnfermedades}/>
+
+        
       </ScrollView>
 
     </View>
 
   );
 }
+
+
+  
+  
+
+       
+
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -124,3 +164,4 @@ const styles = StyleSheet.create({
     padding: 10,
   },
 });
+
